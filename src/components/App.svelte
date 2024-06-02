eddfb48c9c874be434de0a61ef94e9492127aaaa
Samuel Eun <samuel9eun@gmail.com>
2024-06-01 17:42:42 -0700

    finished section 1 and 2

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,12 +1,14 @@
 <script>
 // @ts-nocheck
-    import CoinGraphs from "./CoinGraphs.svelte";
 
     import Scroller from "@sveltejs/svelte-scroller";
     let count, index, offset, progress;
     let top = 0; 
     let threshold = 0.5;
     let bottom = 1;
+
+    import Simple_knn from "$lib/images/simple_knn.png";
+    import CoinGraphs from "./CoinGraphs.svelte";
 
 </script>
 
@@ -20,7 +22,7 @@
     bind:progress
 >
     <nav slot="background">
-        <h1>Visualizing Classification: <br> K-Nearest-Neighbors</h1>
+        <h1 style="font-size: 1.95vw;">Visualizing Classification: <br> K-Nearest-Neighbors</h1>
         <progress 
             value={progress || 0}
             style="width: 21vw; margin: 0 0.5vw;"
@@ -28,53 +30,53 @@
         <ol>
             {#if index == 0}
             <li style="transform: translateY(-{offset*37}vh); font-weight: 600; font-size: 1.6vw;">
-                What is KNN?
+                Classification With KNN
             </li>
             {:else if index > 0}
             <li style="transform: translateY(-37vh);">
-                What is KNN?
+                Classification With KNN
             </li>
             {/if}
 
             {#if index == 1}
             <li style="transform: translateY(-{offset*37}vh); font-weight: 600; font-size: 1.6vw;">
-                Modeling
+                Cryptocurrency Classes
             </li>
             {:else if index > 1}
             <li style="transform: translateY(-37vh);">
-                Modeling
+                Cryptocurrency Classes
             </li>
             {:else}
             <li>
-                Modeling
+                Cryptocurrency Classes
             </li>
             {/if}
 
             {#if index == 2}
             <li style="transform: translateY(-{offset*37}vh); font-weight: 600; font-size: 1.6vw;">
-                Cryptocurrency Classes
+                KNN in Action
             </li>
             {:else if index > 2}
             <li style="transform: translateY(-37vh);">
-                Cryptocurrency Classes
+                KNN in Action
             </li>
             {:else}
             <li>
-                Cryptocurrency Classes
+                KNN in Action
             </li>
             {/if}
 
             {#if index == 3}
             <li style="transform: translateY(-{offset*37}vh); font-weight: 600; font-size: 1.6vw;">
-                KNN in Action
+                Model Tuning
             </li>
             {:else if index > 3}
             <li style="transform: translateY(-37vh);">
-                KNN in Action
+                Model Tuning
             </li>
             {:else}
             <li>
-                KNN in Action
+                Model Tuning
             </li>
             {/if}
 
@@ -90,21 +92,50 @@
         </ol>
     </nav>
     <div slot="foreground">
-        <section>
-            <h1>I. What is KNN?</h1>
+        <section style="height: 64vw;">
+            <h1>I. Classification With KNN</h1>
             <p>
-                KNN, or K-Nearest-Neighbors, is a classification machine learning model.
+                When you hear a new song, you can deduce its genre from the genres of other similar songs you
+                know. Similarly KNN takes a new data point and classifies it by comparing it to the classes of 
+                <i>K</i> most similar data.
+            </p>
+            <img src={Simple_knn} alt="simple knn example"/>
+            <p>
+                I was preparing for a presentation on how machine learning and AI works when I decided 
+                to explain the KNN, K-Nearest-Neighbors, machine learning model. It's an interesting model
+                that derives from the human neurological process of recognition and learning. I used a very 
+                simple visualizer I found online with 20 points of 2 classes differentiated by color. Although 
+                it did the job, it didn't visualize the true strength of machine learning, which is revealed 
+                when applied on large datasets with nuanced associations.
+            </p>
+            <br>
+            <p>
+                Equipped with visualization techniques from DSC 106 with Jard Wilber, I will be revisiting KNN
+                and build a visualization on real cryptocurrency historical price data from the past year.
+            </p>
+        </section>
+        <section style="height: 68vw;">
+            <h1>II. Cryptocurrency Classes</h1>
+            <p>
+                After the boom of cryptocurrency and rise of popularity of NFTs and Meme Stocks/ Meme Coins,
+                there is an ever increasing variety in unique crytocurrency categories. If you take a look
+                at CoinGecko's Top Crypto Categories, you can see a plethora of cryptocurrency classes.
+            </p>
+            <p>
+                Below is a graph representing historical price data of the <i>Layer 1</i>: Bitcoin, Ethereum, 
+                and the like – and <i>Meme</i>: infamous Dogecoin and ShibaInu, etc – classes of cryptocurrency.
+            </p>
+            <CoinGraphs />
+            <p>
+                Let's build a KNN classification model that can classify cryptocurrency as either <b>Layer 1</b>
+                or <b>Meme</b>.
             </p>
         </section>
         <section>
-            <h1>II. Modeling</h1>
+            <h1>III. KNN In Action</h1>
         </section>
         <section>
-            <h1>III. Cryptocurrency Classes</h1>
-            <CoinGraphs />
-        </section>
-        <section>
-            <h1>IV. KNN In Action</h1>
+            <h1>IV. Model Tuning</h1>
         </section>
         <section>
             <h1>V. Demonstration</h1>
@@ -118,10 +149,6 @@
         width: 22vw;
         padding: 1vh 1vw;
         background-color: #ddfdfd;
-    }
-
-    h1 {
-        font-size: 1.95vw;
     }
 
     ol {
@@ -139,18 +166,32 @@
         font-size: 1.3vw;
     }
 
-    /* .one {
-        transform: translateY(-10px);
-    } */
-
     [slot="foreground"] {
         margin-left: 24vw;
     }
 
     section {
-        height: 110vh;
-        padding: 1vw;
+        height: 70vw;
+        padding: 1.5vw;
         margin: 0 0 1vh 0;
         outline: 2vh solid #ddfdfd;
+    }
+
+    h1 {
+        font-size: 2.5vw;
+    }
+
+    p {
+        margin: 1vw 10vw 1vw 5vw;
+
+        text-indent: 2.5vw;
+        font-size: 1.5vw;
+        line-height: 2vw;
+    }
+
+    img {
+        width: 33vw;
+
+        margin: 1vw 20vw;
     }
 </style>
--- src/components/CoinGraphs.svelte
+++ src/components/CoinGraphs.svelte
@@ -2,7 +2,7 @@
 // @ts-nocheck
 
     import * as d3 from 'd3';
-    import { onMount } from 'svelte';
+    import { onDestroy, onMount } from 'svelte';
     import { writable } from 'svelte/store';
     import coin_data from './data/coin_data.json';
     import { browser } from '$app/environment';
@@ -11,6 +11,11 @@
     let formattedData = [];
     const selectedColumns = writable({});
     const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // Set initial dimensions
+    let width;
+    let height;
+    const margin = { top: 20, right: 0, bottom: 20, left: 40 };
   
     if (browser) {
         // Initialize selectedColumns store and format data
@@ -23,8 +28,31 @@
             selectedColumns.set(initialSelection);
 
             formatData();
+            updateDiemensions();
             createChart();
+
+            // Add event listener for window resize
+            window.addEventListener('resize', handleResize);
         });
+
+        // Remove event listener on component destory
+        onDestroy(() => {
+            window.removeEventListener('resize', handleResize);
+        });
+    }
+
+    function updateDiemensions() {
+        width = window.innerWidth * 0.46;
+        height = window.innerWidth * 0.4;
+
+        d3.select('#chart svg')
+            .attr('width', width)
+            .attr('height', height);
+    }
+
+    function handleResize() {
+        updateDiemensions();
+        updateChart($selectedColumns);
     }
   
     function formatData() {
@@ -44,19 +72,14 @@
     }
   
     function createChart() {
-        // Set the dimensions and margins of the graph
-        const margin = { top: 40, right: 60, bottom: 40, left: 60 },
-            width = 900 - margin.left - margin.right,
-            height = 550 - margin.top - margin.bottom;
-    
         const svg = d3.select('#chart')
             .append('svg')
-                .attr('width', width + margin.left + margin.right)
-                .attr('height', height + margin.top + margin.bottom)
+                .attr('width', width)
+                .attr('height', height)
             .append('g')
                 .attr('transform', `translate(${margin.left},${margin.top})`);
     
-        svg.append('g').attr('class', 'x-axis').attr('transform', `translate(0,${height})`);
+        svg.append('g').attr('class', 'x-axis').attr('transform', `translate(0,${height - margin.top - margin.bottom})`);
         svg.append('g').attr('class', 'y-axis');
     }
   
@@ -64,16 +87,12 @@
         if (Object.keys(data).length === 0) {
             return;
         }
-  
-        const margin = { top: 40, right: 60, bottom: 40, left: 60 },
-            width = 900 - margin.left - margin.right,
-            height = 550 - margin.top - margin.bottom;
-  
+
         const svg = d3.select('#chart svg g');
         svg.selectAll('.line').remove();
     
-        const x = d3.scaleTime().range([0, width]);
-        const y = d3.scaleLinear().range([height, 0]);
+        const x = d3.scaleTime().range([0, width - margin.left - margin.right]);
+        const y = d3.scaleLinear().range([height - margin.top - margin.bottom, 0]);
     
         const line = d3.line()
             .x(d => x(d.date))
@@ -89,9 +108,13 @@
             x.domain([new Date(), new Date()]);
             y.domain([0, 1]);
         }
-    
-        svg.select('.x-axis').call(d3.axisBottom(x));
-        svg.select('.y-axis').call(d3.axisLeft(y));
+
+        svg.select('.x-axis')
+            .attr('transform', `translate(0,${height - margin.top - margin.bottom})`)
+            .call(d3.axisBottom(x));
+
+        svg.select('.y-axis')
+            .call(d3.axisLeft(y));
     
         svg.selectAll('.line')
             .data(visibleData)
@@ -104,20 +127,92 @@
             .style('fill', 'none');
     }
 </script>
-  
-<div id="chart"></div>
 
-<div>
-    {#if browser}
-        {#each Object.keys($selectedColumns) as column}
-            <label>
-                <input type="checkbox" bind:checked={$selectedColumns[column]} />
-                {column}
-            </label>
-        {/each}
-    {/if}
-</div>  
+<div id="chart-container">
+    <div id="chart"></div>
+    <div>
+        {#if browser}
+            <div id="select">
+                {#each Object.keys($selectedColumns) as column}
+                    <label>
+                        <input type="checkbox" bind:checked={$selectedColumns[column]} />
+                        <span class="checkbox"></span>
+                        {column}
+                    </label>
+                {/each}
+            </div>
+        {/if}
+    </div>
+</div>
 
 <style>
+    #chart-container {
+        display: flex;
+    }
 
+    #chart {
+        margin: 1vw 4vw 1vw 5vw;
+    }
+
+    #select {
+        display: flex;
+        flex-direction: column;
+
+        padding: 1.5vw 0;
+    }
+
+    label {
+        display: block;
+        position: relative;
+
+        padding: 0.2vw 1.8vw;
+
+        font-size: 1.3vw;
+    }
+
+    input {
+        position: absolute;
+        height: 1.4vw;
+        width: 1.4vw;
+        opacity: 0;
+    }
+
+    .checkbox {
+        position: absolute;
+        top: 3px;
+        left: 0;
+        height: 1.4vw;
+        width: 1.4vw;
+        background-color: #ddd;
+    }
+
+    label:hover input ~ .checkbox {
+        background-color: #bbb;
+    }
+
+    label input:checked ~ .checkbox {
+        background-color: #2196F3;
+    }
+
+    .checkbox:after {
+        content: "";
+        position: absolute;
+        display: none;
+    }
+
+    label input:checked ~ .checkbox:after {
+        display: block;
+    }
+
+    label .checkbox:after {
+        left: 0.43vw;
+        top: 0.2vw;
+        width: 0.3vw;
+        height: 0.6vw;
+        border: solid white;
+        border-width: 0 2px 2px 0;
+        -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
+    }
 </style>
